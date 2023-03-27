341ceb22131d2a57a2b4cfd283690ee2a63e44fc
zzzzz <ricardopinto15@hotmail.com>
2023-03-27 04:22:49 +0100

    web scraping working

--- analyser.js
+++ analyser.js
@@ -1,12 +1,13 @@
+//import "./findAndReplaceDOMText.js"
+
 const DEBUG = true;
-function logDebug(msg)
-{
+function logDebug(msg) {
    if (DEBUG)
       console.log(msg);
 }
 
 function analysePage() {
-   const relevantTags = ["div", "p", "span", "h1", "h2", "h3", "h4", "h5", "h6"];
+   const relevantTags = ["div", "p", "span", "h1", "h2", "h3", "h4", "h5", "h6", "b"];
    const jquerySearch = relevantTags.join(", "); // "div, p, span, h1, h2, h3, h4, h5, h6"
 
    const URL = "http://localhost:8000/api/v1";
@@ -38,42 +39,48 @@ function analysePage() {
 
       let txt = $(this).text().replace(/ +/g, ' ').trim(); // text in this element, replace many spaces with 1 space
       //txt = txt.replace(/(\r\n|\n|\r)/gm, "").trim(); // remove all line breaks
-      //txt = $(this).text().replace(/ +/g, ' ').trim(); // text in this element, replace many spaces with 1 space
       let sentences = textToSentences(txt);
 
       if (sentences == null || sentences == undefined) {
          // If failed to split into sentences, then treat the text as a whole
          txt = txt.replace(/(\r\n|\n|\r)/gm, "").trim(); // remove all line breaks
-         myFetch($(this)[0], txt);
+         myFetch(txt, $(this)[0]);
       }
       else {
          for (let i = 0; i < sentences.length; i++) {
-            myFetch($(this)[0], sentences[i]);
+            myFetch(sentences[i], $(this)[0]);
          }
       }
    });
 
-function textToSentences(txt) {
-   // Considers all line breaks except for enter key line breaks
+   function textToSentences(text) {
+      let sentences = text.replace(/(\.+|\:|\!|\?)(\"*|\'*|\)*|}*|]*)(\s|\n|\r|\r\n)/gm, "$1$2|").split("|");
+      let indexToList = new Map();
+      for (let i = 0; i < sentences.length; i++) {
+         sentences[i] = sentences[i].trim();
+         sentences[i] = sentences[i].replace(/ +/g, ' ').trim(); // replace many spaces with 1 space
+         let splitBrBreaks = sentences[i].split("\n \n");
+         if (splitBrBreaks.length > 1) {
+            sentences[i] = splitBrBreaks[0];
+            indexToList.set(i + 1, splitBrBreaks.slice(1));
+         }
+      }
+      let x = 0;
+      for (i of indexToList.keys()) {
+         sentences.splice(i + x, 0, ...indexToList.get(i));
+         x += indexToList.get(i).length;
+      }
+      for (let i = 0; i < sentences.length; i++) {
+         sentences[i] = sentences[i].trim();
+         sentences[i] = sentences[i].replace(/ +/g, ' ').trim(); // replace many spaces with 1 space
+         sentences[i] = sentences[i].replaceAll("\n", "");
+      }
 
-  // Replace enter breaks with a space
-  txt = txt.replace(/\n/g, ' ');
-  
-  // Match sentences with sentence-ending punctuation or <br> tags
-  let sentences = txt.match(/[^\.!\?]+[\.!\?]+(<br\s*\/?>)?\s*/g) || [];
-  
-  // If input text does not end with sentence-ending punctuation, add last sentence
-  if (txt.match(/[^\.!\?]+\s*$/)) {
-    let lastSentence = txt.match(/[^\.!\?]+\s*$/)[0];
-    sentences.push(lastSentence);
-  }
-  
-  // Trim whitespace from each sentence
-  sentences = sentences.map(sentence => sentence.trim());
-  
-  return sentences;
-}
-   function myFetch(elem, text) {
+      return sentences;
+   }
+
+
+   function myFetch(text, elem) {
       let promise = fetch(URL, {
          method: "POST",
          headers: {
@@ -107,7 +114,25 @@ function textToSentences(txt) {
                });
                */
 
-               highlightText(text);
+               //highlightText(text, elem);
+
+               let newText = text.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // ignore special chars
+               newText = newText.replace(/\s+/g, '\\s+');  // replace whitespace with \s+ pattern, this matches even with many spaces or line breaks between words
+               let pattern = RegExp("\\b" + newText + "\\b");
+               let before = elem.innerHTML;
+               findAndReplaceDOMText(elem, {
+                  find: pattern,
+                  wrap: 'span',
+                  wrapClass: 'myMark',
+               });
+               if (elem.innerHTML == before) {
+                  pattern = RegExp(newText);
+                  findAndReplaceDOMText(elem, {
+                     find: pattern,
+                     wrap: 'span',
+                     wrapClass: 'myMark',
+                  });
+               }
 
                sumCharacters += text.length;
                weightedSum += text.length * data.probability_AI_generated;
@@ -119,37 +144,36 @@ function textToSentences(txt) {
    }
 
    return new Promise((resolve, reject) => {
-    Promise.all(promises).then(() => {
-      // all fetches done
-      let weightedAvg = weightedSum / sumCharacters;
-      weightedAvg = Math.round(weightedAvg); // round to nearest int
-      console.log("Overall evaluation: " + weightedAvg + "%");
-      //alert("Overall evaluation: " + weightedAvg + "%");
-      resolve(weightedAvg); // resolve the new Promise with the value of weightedAvg
-    }).catch((err) => {
-      reject(err); // if any of the Promises in the Promise.all() chain reject, reject the new Promise with the same error
-    });
-  });
+      Promise.all(promises).then(() => {
+         // all fetches done
+         let weightedAvg = weightedSum / sumCharacters;
+         weightedAvg = Math.round(weightedAvg); // round to nearest int
+         console.log("Overall evaluation: " + weightedAvg + "%");
+         //alert("Overall evaluation: " + weightedAvg + "%");
+         resolve(weightedAvg); // resolve the new Promise with the value of weightedAvg
+      }).catch((err) => {
+         reject(err); // if any of the Promises in the Promise.all() chain reject, reject the new Promise with the same error
+      });
+   });
 }
 
 // https://stackoverflow.com/questions/49565116/javascript-need-help-in-highlight-across-tags-text-in-html
-function highlightText(text)
-{
+function highlightText(text, elem = document.body) {
    //text = text.replace(/(\s+)/,"(<[^>]+>|[\\r\\n\\s]+)*");
-   text = text.split(' ').join('(<[^>]+>|[\\r\\n\\s]+)*')
+   text = text.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // add an '\' before every regex symbol
+   text = text.split(' ').join('(<[^>]+>|[\\r\\n\\s]+)*');
    //alert(text);
-   text = text.replace(/\?/g, "\\?");
-   var pattern = new RegExp("("+text+")", "gi");
+   var pattern = new RegExp("(" + text + ")", "gi");
 
-   document.body.innerHTML = document.body.innerHTML.replace(pattern, function(match){
-   var re = /(^|[>])(?![^<]*?<\/y)(.*?)([<]|$)/gi; 
-   match = match.replace(/(\r\n\t|\n|\r\t)/gm,"");
+   elem.innerHTML = elem.innerHTML.replace(pattern, function (match) {
+      var re = /(^|[>])(?![^<]*?<\/y)(.*?)([<]|$)/gi;
+      match = match.replace(/(\r\n\t|\n|\r\t)/gm, "");
 
-   match = match.replace(re, function (m,g1,g2,g3) {
-     return g1+"<span style='background-color:red'>"+g2+"</span>"+g3;
-   });
-   //alert(match);
-   return match;
+      match = match.replace(re, function (m, g1, g2, g3) {
+         return g1 + "<span style='background-color:red'>" + g2 + "</span>" + g3;
+      });
+      //alert(match);
+      return match;
    });
 }
 
@@ -157,23 +181,21 @@ function highlightText(text)
 // https://stackoverflow.com/questions/19710158/jquery-highlight-pieces-of-text-in-an-element-across-tags
 function highlightText2(text) {
    if (window.find && window.getSelection) {
-       document.designMode = "on";
-       var sel = window.getSelection();
-       sel.collapse(document.body, 0);
-       while (window.find(text)) {
-            console.log("found txt");
-           //document.getElementById("button").blur();
-           document.execCommand("HiliteColor", false, "red");
-           sel.collapseToEnd();
-       }
-       document.designMode = "off";
+      document.designMode = "on";
+      var sel = window.getSelection();
+      sel.collapse(document.body, 0);
+      while (window.find(text)) {
+         //document.getElementById("button").blur();
+         document.execCommand("HiliteColor", false, "red");
+         sel.collapseToEnd();
+      }
+      document.designMode = "off";
    } else if (document.body.createTextRange) {
-      console.log("haha");
-       var textRange = document.body.createTextRange();
-       while (textRange.findText(text)) {
-           textRange.execCommand("BackColor", false, "red");
-           textRange.collapse(false);
-       }
+      var textRange = document.body.createTextRange();
+      while (textRange.findText(text)) {
+         textRange.execCommand("BackColor", false, "red");
+         textRange.collapse(false);
+      }
    }
 }
 
@@ -210,7 +232,7 @@ function makeRegexp(s) {
    var result = s.replace(/\s+/gi, space);
    result = new RegExp(space + result + space, 'gi');
    return result;
- }
+}
 
 function emulateSelection(htmlPiece) {
    htmlPiece = htmlPiece.replace(/(?!=>)[^><]+(?=<)/g, function (w) {
@@ -234,21 +256,21 @@ function emulateSelection(htmlPiece) {
 }
 
 function wrapWords(plainPiece) {
-  var start = '<span search="xxxxx">',
+   var start = '<span search="xxxxx">',
       stop = '</span>';
 
-  var regex = /<[^>]*>|[^<]+/gi;
-  var matches = plainPiece.match(regex);
-  var result = '';
-  for (var i = 0; i < matches.length; i++) {
-    var match = matches[i];
-    if (match.charAt(0) === '<') {
-      result += match;
-    } else {
-      result += start + match + stop;
-    }
-  }
+   var regex = /<[^>]*>|[^<]+/gi;
+   var matches = plainPiece.match(regex);
+   var result = '';
+   for (var i = 0; i < matches.length; i++) {
+      var match = matches[i];
+      if (match.charAt(0) === '<') {
+         result += match;
+      } else {
+         result += start + match + stop;
+      }
+   }
 
-  return result;
+   return result;
 }
 
