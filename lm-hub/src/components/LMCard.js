ee009a8e4bb9e0dbd6bbc2a4d2439c53714c0662
João Matos <joaorpm02@ua.pt>
2023-03-28 09:16:13 +0100

    Updated LM Hub

--- lm-hub/src/components/LMCard.js
+++ lm-hub/src/components/LMCard.js
@@ -1,8 +1,33 @@
 import { useState } from "react";
+import React from 'react';
 
 
 function LMCard( props ) {
     const [navbar, setLMCard] = useState(false);
+
+    const [like, setLike] = useState(0),
+    [isLike, setIsLike] = useState(false),
+
+    onLikeButtonClick = () => {
+        setLike(like + (isLike ? -1 : 1));
+        setIsLike(!isLike);
+        if (isDisLike) {
+            setDislike(dislike + (isDisLike ? -1 : 1));
+            setIsDislike(!isDisLike);
+        }
+    }
+
+    const [dislike, setDislike] = useState(0),
+    [isDisLike, setIsDislike] = useState(false),
+
+    onDislikeButtonClick = () => {
+        setDislike(dislike + (isDisLike ? -1 : 1));
+        setIsDislike(!isDisLike);
+        if (isLike) {
+            setLike(like + (isLike ? -1 : 1));
+            setIsLike(!isLike);
+        }
+    }
 
     return (
         <div className="card card-side bg-base-100 shadow-xl pt-100 border-gray-300 justify-content">
@@ -11,10 +36,10 @@ function LMCard( props ) {
                 <h2 className="card-title">Language Model!</h2>
                 <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
                 <div className="card-actions justify-end">
-                <button type="button" class="text-red-700 border border-red-700 hover:bg-red-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800 dark:hover:bg-red-500">
-                    <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" clip-rule="evenodd"></path></svg>
-                    <span class="sr-only">Icon description</span>
-                </button>
+                    <i style={{fontSize: "30px", paddingTop: "25px", color: (isLike ? "green" : "black")}} class="fa fa-thumbs-o-up" onClick={onLikeButtonClick} ></i>
+                    <p style={{paddingTop: "28px"}}>{like}</p>
+                    <i style={{fontSize: "30px", paddingTop: "25px", color: (isDisLike ? "red" : "black")}} class="fa fa-thumbs-o-down" onClick={onDislikeButtonClick} ></i>
+                    <p style={{paddingTop: "28px"}}>{dislike}</p>
                     {/* The button to open modal */}
                     <label htmlFor="my-modal2" className="btn btn-primary">See More</label>
 
@@ -36,3 +61,5 @@ function LMCard( props ) {
 }
 
 export default LMCard;
+
+
