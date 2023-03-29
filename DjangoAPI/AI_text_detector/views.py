3239169f8a022c69110bfa7fd4c99284a3779c65
zzzzz <ricardopinto15@hotmail.com>
2023-03-29 17:04:06 +0100

    Cleaned API

--- DjangoAPI/AI_text_detector/views.py
+++ DjangoAPI/AI_text_detector/views.py
@@ -5,11 +5,9 @@ from django.views.decorators.csrf import csrf_exempt
 import random
 import json
 from .LMs.AI import LanguageModel
-from .webPageScraper import *
 from datetime import datetime
-from bs4 import BeautifulSoup
 
-model = LanguageModel()
+#model = LanguageModel()
 
 def log(msg):
     strDateTimeNow = str(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
@@ -21,71 +19,28 @@ def handle_request(request):
     # 400 means bad request and 404 means resource not found
     # 500 means an internal server error
 
-    if "url" in request.GET:
-        log("Received url request for \"" + request.GET["url"] + "\"")
-        return handle_web_page_request(request, "url")
-    elif request.content_type == 'text/plain':
-        txt = request.body.decode("utf-8")
-        log("Received text request for \"" + txt + "\"")
-        return handle_text_request(request)
-    elif request.content_type == 'text/html':
-        html = request.body.decode("utf-8")
-        soup = BeautifulSoup(html, 'html.parser')
-        if soup.title and soup.title.string:
-            log("Received html request for " + soup.title.string)
-        else:
-            log("Received html request")
-        return handle_web_page_request(request, "html")
+    if request.content_type == 'text/html':
+        text = request.body.decode("utf-8")
     else:
-        return JsonResponse(
-            {'message': "Invalid request"},
-            status=400,
-            json_dumps_params={'indent': 2})
+        try:
+            requestData = json.loads(request.body.decode())
+            text = requestData["text"];
+        except:
+            log("Received invalid request")
+            return JsonResponse(
+                {'message': "Invalid request"},
+                status=400,
+                json_dumps_params={'indent': 2})
 
-
-def handle_text_request(request):
-    text = request.body.decode("utf-8")
+    log("Received text request for \"" + text + "\"")
 
     #probability_AI_generated = model.probability_AI_generated_text(text, "openAIBase")
-    probability_AI_generated = random.randint(1,99)
+    probability_AI_generated = random.randint(0, 100)
 
     responseData = {
         "text": text,
         "probability_AI_generated": probability_AI_generated
     }
-
-    return JsonResponse(
-        responseData,
-        status=200,
-        json_dumps_params={'indent': 2})
-
-def handle_web_page_request(request, type):
-
-    if type == "url":
-        selectorsAndText, htmlHash = getSelectorsAndTextFromURL(request.GET["url"])
-    else:
-        assert type == "html"
-        selectorsAndText, htmlHash = getSelectorsAndTextFromHtml(request.body.decode("utf-8"))
-
-    responseData = {}
-
-    responseData["html_checksum"] = htmlHash
-
-    responseData["analysis"] = selectorsAndText
-    length = len(selectorsAndText)
-    sumCharacters = 0
-    weightedSum = 0
-    for i in range(length):
-        text = selectorsAndText[i]["text"]
-        #probability_AI_generated = model.probability_AI_generated_text(text, "openAIBase")
-        probability_AI_generated = random.randint(1,99)
-        responseData["analysis"][i]["probability_AI_generated"] = probability_AI_generated
-        sumCharacters += len(text)
-        weightedSum += len(text) * probability_AI_generated
-        #print(str(i+1) + "/" + str(length) + " calculated")
-
-    weightedAverage = weightedSum / sumCharacters
-    responseData["overall_evaluation"] = round(weightedAverage)
 
     return JsonResponse(
         responseData,
