d558460c06f7230c61c9217fec8ba8014860ebf9
leina <djbf@ua.pt>
2023-04-03 00:09:43 +0100

    Adjusted styles of single card component

--- ai4-td-extension/components/single-card/index.tsx
+++ ai4-td-extension/components/single-card/index.tsx
@@ -1,7 +1,21 @@
+import { Box } from "@mui/material";
+
 function SingleCard(props) {
     
     return (
-        <div>HELLO</div>
+        <Box sx={{
+            backgroundColor: 'background.paper',
+            color: 'rgba(0, 0, 0, 0.87)',
+            transition: 'box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+            borderRadius: '4px',
+            boxShadow: 'rgba(0, 0, 0, 0.2) 0px 5px 5px -3px, rgba(0, 0, 0, 0.14) 0px 8px 10px 1px, rgba(0, 0, 0, 0.12) 0px 3px 14px 2px',
+            overflow: 'hidden auto',
+            outline: '0px',
+            padding: '16px',
+            fontSize: '14px',
+        }}>
+            HELLO
+        </Box>
     );
 }
 
