f7c487c4e28d5db87b5497e0eec2c508fee57dda
leina <djbf@ua.pt>
2023-04-04 17:42:47 +0100

    Fixed some shit, cleaned some code, and also implemented reload scan (not very tested so might have bugs)

--- ai4-td-extension/components/global-card/index.tsx
+++ ai4-td-extension/components/global-card/index.tsx
@@ -1,24 +1,27 @@
-import { Box } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
+import { IoReloadCircle } from "react-icons/io5";
 
-function GlobalCard({data, success, anchor, setAnchor}) {
+function GlobalCard({data, onReloadClick}) {
 
     return (
-        <Box sx={{
-            display: (success && anchor)? "initial": "none",
-            position: 'fixed',
-            right: 30,
-            bottom: 100,
-            backgroundColor: 'background.paper',
-            color: 'rgba(0, 0, 0, 0.87)',
-            transition: 'box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
-            borderRadius: '4px',
-            boxShadow: 'rgba(0, 0, 0, 0.2) 0px 5px 5px -3px, rgba(0, 0, 0, 0.14) 0px 8px 10px 1px, rgba(0, 0, 0, 0.12) 0px 3px 14px 2px',
-            overflow: 'hidden auto',
-            outline: '0px',
-            padding: '16px',
-            fontSize: '14px',
-        }}>
-            {`Overall evaluation ${data}%`}
+        <Box 
+            width={'200px'} 
+            fontSize={'14px'} 
+            padding={'16px'} 
+            borderRadius={'4px'} 
+            overflow={'hidden auto'} 
+            boxShadow={'rgba(0, 0, 0, 0.2) 0px 5px 5px -3px, rgba(0, 0, 0, 0.14) 0px 8px 10px 1px, rgba(0, 0, 0, 0.12) 0px 3px 14px 2px'} 
+            color={'rgba(0, 0, 0, 0.87)'} 
+            bgcolor={'background.paper'} 
+            bottom={100} 
+            right={30} 
+            position={'fixed'} 
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'space-between'}
+        >
+            <span>{`Overall evaluation ${data}%`}</span>
+            <IconButton onClick={onReloadClick} sx={{padding: 0}} children={<IoReloadCircle size={32} color="#8b8bec"/>} />
         </Box>
     )
 }
