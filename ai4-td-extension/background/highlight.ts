fb458f4f62bc64fb49a74ee4ea9bc5eae3a2564d
leina <djbf@ua.pt>
2023-04-06 18:00:09 +0100

    highlight selected text not integrated with api yet

--- ai4-td-extension/background/highlight.ts
+++ ai4-td-extension/background/highlight.ts
@@ -1,4 +1,4 @@
-function highlightSelection(selectionText: string) {
+function highlightSelection(selectionText: string, keep = false, merge = true) {
 
   const getNodeRange = (range, node) => {
     const nodeRange = document.createRange();
@@ -44,19 +44,80 @@ function highlightSelection(selectionText: string) {
   
     return selectedNodes;
   }
+
+  function createHighlightElement(prob) {
+    const highlight = document.createElement('highlighted-text');
+    highlight.setAttribute("probability", prob);
+    return highlight;
+  }
+
+  function canMerge(element1, element2) {
+    return element1.nodeName === "HIGHLIGHTED-TEXT" && element2.nodeName === "HIGHLIGHTED-TEXT" && element1.getAttribute("probability") === element2.getAttribute("probability")
+  }
+
+  function mergeSiblingHighlights(element) {
+    const children = Array.from(element.children);
+    children.forEach((child: HTMLElement, i) => {
+      if (i === 0) {
+        return;
+      }
+      const prevChild = children[i - 1] as HTMLElement;
+      if (canMerge(prevChild, child)) {
+        const mergedText = prevChild.textContent + child.textContent;
+        prevChild.textContent = mergedText;
+        element.removeChild(child);
+      }
+    });
+  }
+
+  function removeNestedHighlights(element) {
+    const content = element.innerHTML;
+    const fragment = document.createRange().createContextualFragment(content);
+    element.replaceWith(fragment);
+  }
+
+  function keepNestedHighlights(element) {
+    const childElements = Array.from(element.childNodes);
+    childElements.forEach((child: HTMLElement) => {
+      if (child.nodeName === 'HIGHLIGHTED-TEXT') {
+        if (child.compareDocumentPosition(element) & Node.DOCUMENT_POSITION_FOLLOWING) {
+          element.parentNode.insertBefore(child, element.nextSibling);
+        } else {
+          element.parentNode.insertBefore(child, element);
+        }
+      }
+    });
+  }
+
+  function refactorNestedHighlights(element) {
+    if (keep) {
+      keepNestedHighlights(element);
+    }
+    else {
+      removeNestedHighlights(element)
+    }
+    if (merge) {
+      mergeSiblingHighlights(element.parentNode)
+    }
+  }
   
   const range = window.getSelection().getRangeAt(0);
   const selectedNodes = getSelectedNodes(range);
+  const prob = String(Math.round(Math.random() * 50 +  50));
 
   selectedNodes.forEach((node) => {
-    const highlight = document.createElement('highlighted-text');
-    highlight.setAttribute("probability", "50");
+    const highlight = createHighlightElement(prob);
     const nodeRange = getNodeRange(range, node);
     highlight.textContent = nodeRange.toString();
 
     nodeRange.deleteContents();
     nodeRange.insertNode(highlight);
-  })
-}
 
+    const parent = highlight.parentNode;
+    if (parent.nodeName === "HIGHLIGHTED-TEXT") {
+      refactorNestedHighlights(parent)
+    }
+  });
+}
+  
 export default highlightSelection;
