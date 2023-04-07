1675fbb6d054c7809bcafad18e63caceaf30cdd3
leina <djbf@ua.pt>
2023-04-07 03:05:00 +0100

    Authentication passing every test and giving always responses

--- DjangoAPI/authentication/models.py
+++ DjangoAPI/authentication/models.py
@@ -1,10 +1,10 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.contrib.auth.base_user import BaseUserManager
 
+
 class AppUserManager(BaseUserManager):
-    
-    def create_user(self, username, email,password = None, **extra_fields):
+
+    def create_user(self, username, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Email is required')
         if not password:
@@ -12,14 +12,13 @@ class AppUserManager(BaseUserManager):
         if not username:
             raise ValueError('Username is required')
         email = self.normalize_email(email)
-        user = self.model(email = email, username = username, **extra_fields)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save()
         return user
-    
-    def create_superuser(self, username, email, password = None, **extra_fields):
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         user = self.create_user(username, email, password, **extra_fields)
         return user
-        
--- DjangoAPI/authentication/tests.py
+++ DjangoAPI/authentication/tests.py
@@ -1,69 +1,60 @@
-from django.test import TestCase, Client
 from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
-import json
-import os
-from django.conf import settings
-os.environ['DJANGO_SETTINGS_MODULE'] = 'DjangoAPI.settings'
-settings.configure()
 
 
-class TestViews(TestCase):
-    
+class UserRegisterTestCase(APITestCase):
+    def test_register_user(self):
+        url = reverse('register')
+        data = {'username': 'test_user', 'email': 'test@test.com', 'password': 'test1234'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'test_user')
+
+
+class UserLoginLogoutTestCase(APITestCase):
     def setUp(self):
-        self.client = Client()
-        self.register_url = reverse('register')
-        self.login_url = reverse('login')
-        self.valid_payload = {
-            'username': 'testuser',
-            'email': 'testuser@example.com',
-            'password': 'testpassword'
-        }
-        self.invalid_payload = {
-            'username': '',
-            'email': 'testuser@example.com',
-            'password': 'testpassword'
-        }
-        self.user = User.objects.create_user(
-            username='testuser', 
-            email='testuser@example.com',
-            password='testpassword'
-        )
+        self.user = User.objects.create_user('test_user', 'test@test.com', 'test1234')
 
-    def test_register_view(self):
-        response = self.client.post(
-            self.register_url,
-            data=json.dumps(self.valid_payload),
-            content_type='application/json'
-        )
-        self.assertEqual(response.status_code, 201)
+    def test_login_user_with_correct_credentials(self):
+        url = reverse('login')
+        data = {'email': 'test@test.com', 'password': 'test1234'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue('sessionid' in response.cookies)
 
-        response = self.client.post(
-            self.register_url,
-            data=json.dumps(self.invalid_payload),
-            content_type='application/json'
-        )
-        self.assertEqual(response.status_code, 400)
+    def test_login_user_with_incorrect_credentials(self):
+        url = reverse('login')
+        data = {'email': 'test@test.com', 'password': 'wrongpassword'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertFalse('sessionid' in response.cookies)
 
-    def test_login_view(self):
-        response = self.client.post(
-            self.login_url,
-            data=json.dumps({
-                'email': 'testuser@example.com',
-                'password': 'testpassword'
-            }),
-            content_type='application/json'
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('access' in response.json())
-        self.assertTrue('refresh' in response.json())
+    def test_logout_user(self):
+        self.client.login(email='test@test.com', password='test1234')
+        url = reverse('logout')
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse('sessionid' in response.cookies)
 
-        response = self.client.post(
-            self.login_url,
-            data=json.dumps({
-                'email': 'testuser@example.com',
-                'password': 'wrongpassword'
-            }),
-            content_type='application/json'
-        )
-        self.assertEqual(response.status_code, 401)
+
+class UserViewTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('test_user', 'test@test.com', 'test1234')
+        url = reverse('login')
+        data = {'email': 'test@test.com', 'password': 'test1234'}
+        response = self.client.post(url, data, format='json')
+        self.assertTrue(self.user.is_authenticated)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue('sessionid' in response.cookies)
+        self.cookie = self.client.cookies.get('sessionid')
+
+    def test_get_user(self):
+        url = reverse('user')
+        headers = {'Cookie': f'sessionid={self.cookie.value}'}
+        response = self.client.get(url, headers=headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['user']['username'], 'test_user')
+        self.assertTrue('sessionid' in response.cookies)
