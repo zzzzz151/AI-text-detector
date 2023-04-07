11923d10c1683b28da88bfa26280fe6e01f4790b
leina <djbf@ua.pt>
2023-04-07 15:47:16 +0100

    fixed conflicts to pass the tests

--- DjangoProject/authentication/tests.py
+++ DjangoProject/authentication/tests.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class UserRegisterTestCase(APITestCase):
     def test_register_user(self):
-        url = reverse('register')
+        url = reverse('authentication:register')
         data = {'username': 'test_user', 'email': 'test@test.com', 'password': 'test1234'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -19,28 +19,28 @@ class UserLoginLogoutTestCase(APITestCase):
         self.user = User.objects.create_user('test_user', 'test@test.com', 'test1234')
 
     def test_login_user_with_correct_credentials_email(self):
-        url = reverse('login')
+        url = reverse('authentication:login')
         data = {'user_id': 'test@test.com', 'password': 'test1234'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue('sessionid' in response.cookies)
 
     def test_login_user_with_correct_credentials_username(self):
-        url = reverse('login')
+        url = reverse('authentication:login')
         data = {'user_id': 'test_user', 'password': 'test1234'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue('sessionid' in response.cookies)
 
     def test_login_user_with_incorrect_credentials(self):
-        url = reverse('login')
+        url = reverse('authentication:login')
         data = {'user_id': 'test@test.com', 'password': 'wrongpassword'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertFalse('sessionid' in response.cookies)
 
     def test_login_user_with_incorrect_format(self):
-        url = reverse('login')
+        url = reverse('authentication:login')
         data = {'teste': 'teste', 'teste2': 'teste2'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -48,7 +48,7 @@ class UserLoginLogoutTestCase(APITestCase):
 
     def test_logout_user(self):
         self.client.login(email='test@test.com', password='test1234')
-        url = reverse('logout')
+        url = reverse('authentication:logout')
         response = self.client.post(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertFalse('sessionid' in response.cookies)
@@ -57,7 +57,7 @@ class UserLoginLogoutTestCase(APITestCase):
 class UserViewTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user('test_user', 'test@test.com', 'test1234')
-        url = reverse('login')
+        url = reverse('authentication:login')
         data = {'user_id': 'test@test.com', 'password': 'test1234'}
         response = self.client.post(url, data, format='json')
         self.assertTrue(self.user.is_authenticated)
@@ -66,7 +66,7 @@ class UserViewTestCase(APITestCase):
         self.cookie = self.client.cookies.get('sessionid')
 
     def test_get_user(self):
-        url = reverse('user')
+        url = reverse('authentication:user')
         headers = {'Cookie': f'sessionid={self.cookie.value}'}
         response = self.client.get(url, headers=headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
