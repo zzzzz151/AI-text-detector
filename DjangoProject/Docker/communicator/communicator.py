1a35c4d80540f824067b3a1f0227174285b86e0d
Xela10001 <alexandre.gazur@ua.pt>
2023-04-11 10:18:05 +0100

    stuff

---
+++ DjangoProject/Docker/communicator/communicator.py
@@ -0,0 +1,72 @@
+import sys
+import lm_submission as lm
+import socket
+import os
+
+import messages as m
+
+
+def log(text):
+	print(text)
+	sys.stdout.flush()
+
+class IsolatedLanguageModel:
+	def __init__(self, lm_name):
+		# Warning: Takes time to load
+		log("Loading model")
+		self.model = lm.LanguageModel()
+		self.name = lm_name
+
+	def predict(self, text:str):
+		result = self.model.predict(text)
+		if type(result) == list:
+			result = result[0]
+		return round(result * 100)
+
+def handle_messages(server_socket, message, model):
+	if not message:
+		return
+
+	messageID = message.identification
+	messageType = message.message_type
+
+	if messageType == 'connect':
+		...
+	elif messageType == 'predict':
+		probability = model.predict(message.text)
+		m.send_message_object(server_socket, m.create_probability_message(messageID, model.name, probability))
+
+
+if __name__ == "__main__":
+	try:
+		port = int(os.getenv('LISTEN_PORT'))
+	except TypeError:
+		port = int(sys.argv[2])
+	try:
+		host = os.getenv('LISTEN_HOST')
+		if not host:
+			host = sys.argv[3]
+	except TypeError:
+		host = sys.argv[3]
+
+	try:
+		lm_name = os.getenv('LM_NAME')
+	except TypeError:
+		try:
+			lm_name = sys.argv[4]
+		except IndexError:
+			lm_name = "abcd"
+	log(f"NAME: {lm_name}")
+	model = IsolatedLanguageModel(lm_name)
+
+	log(f"Connecting LM {lm_name} with port {port}")
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.connect((host, port))
+		m.send_message_object(s, m.create_connect_message(lm_name))
+		while True:
+			message = m.receive_message_object(s)
+			if not message:
+				log("Server has closed")
+				exit(1)
+
+			handle_messages(s, message, model)
---
+++ DjangoProject/Docker/test.py
@@ -0,0 +1,143 @@
+from time import sleep
+"""
+import subprocess
+import docker_compose
+
+# Load the Docker Compose file
+compose_file = 'path/to/docker-compose.yml'
+project = docker_compose.Project.from_file(compose_file)
+
+compose_file_path = './communicator/docker-compose.yml'
+
+var = 'LISTEN_PORT=3131'
+
+for i in range(5):
+    env_var = f"MY_ENV_VAR_{i}"
+    subprocess.run(["docker-compose", '-f', compose_file_path, "up", "-d", "--scale", "communicator=5", "--no-recreate"], env={env_var: "some_value"})
+
+
+
+subprocess.run(['docker-compose', '-f', compose_file_path, 'up', 'communicator', '--detach'], env={'LISTEN_PORT': '3131'})
+sleep(10)
+subprocess.run(['docker-compose', '-f', compose_file_path, 'stop', 'communicator'])
+subprocess.run(['docker-compose', '-f', compose_file_path, 'stop', 'server'])
+"""
+
+import subprocess
+import docker
+import os
+
+service_name = "communicator"
+
+def container_exists(client, container_name:str):
+    containers = client.containers.list(all=True)
+    container_names = [c.name for c in containers]
+    return container_name in container_names
+def container_is_running(client, container_name:str):
+    containers = client.containers.list(all=False)
+    container_names = [c.name for c in containers]
+    return container_name in container_names
+def create_container_name(service_name, lm_name):
+    return f"{service_name}_{lm_name}"
+def build_image(compose_file_path:str, build_args:dict):
+    command = [
+        "docker", "compose",
+        "-f", compose_file_path,
+        "build",
+        "--quiet"
+    ]
+    for env_name, env_value in build_args.items():
+        command.append("--build-arg")
+        command.append(f"{env_name}={env_value}")
+
+    subprocess.run(command, check=True)
+    """
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT, universal_newlines=True)
+        print(output)
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+    """
+def run_image(compose_file_path:str, service_name:str, container_name:str, extra_options:list):
+    command = [
+        "docker", "compose",
+        "-f", compose_file_path,
+        "run",
+        "-d",
+        '--name', container_name,
+        service_name,
+    ]
+
+    for arg_tuple in extra_options:
+        indicator = arg_tuple[0]
+        if indicator == 'e':
+            command.insert(5, f"-{indicator}")
+            command.insert(6, f"{arg_tuple[1]}={arg_tuple[2]}")
+        elif indicator == 'v':
+            command.insert(5, f"-{indicator}")
+            command.insert(6, f"{arg_tuple[1]}")
+        else:
+            for arg in arg_tuple:
+                command.append(arg)
+
+
+    subprocess.run(command, check=True)
+def start_container(container_name):
+    subprocess.run(['docker', 'start', container_name])
+def create_or_run_container(client, compose_file_path:str, service_name:str, container_name:str, extra_options:list):
+    if not container_exists(client, container_name):
+        build_image(compose_file_path, {"LM_NAME":extra_options[0][2]})
+        run_image(compose_file_path, service_name, container_name, extra_options)
+    elif not container_is_running(client, container_name):
+        start_container(container_name)
+def store_file(path, file_name, file):
+    os.makedirs(path, exist_ok=True)
+    newFile = open(file_name, "w")
+    newFile.write(file.read().decode('UTF-8'))
+    newFile.close()
+def add_communicator(compose_file_path, lm_name):
+    client = docker.from_env()
+    env_vars = [
+        ('e', "LM_NAME", lm_name),
+        #('v', f"/DjangoAPI/AI_text_detector/LMs/{lm_name}:/usr/app/src/lm_name"),
+        #('v', "/DjangoAPI/Docker/communicator/communicator.py:/usr/app/src/communicator.py"),
+        #('v', "/DjangoAPI/Docker/communicator/messages.py:/usr/app/src/messages.py"),
+        #('pip3', 'install', '-r', f'/usr/app/src/lm_name/requirements.txt')
+    ]
+    create_or_run_container(client, compose_file_path, service_name, create_container_name(service_name, lm_name), env_vars)
+    client.close()
+def start_communicator(lm_name):
+    client = docker.from_env()
+    container_name = create_container_name(service_name, lm_name)
+    if not container_is_running(client, container_name):
+        start_container(container_name)
+    client.close()
+
+
+if __name__ == "__main__":
+    ...
+    """
+    client = docker.from_env()
+
+    compose_file_path = './communicator/docker-compose.yml'
+    env_vars = [
+        ('e', "LISTEN_PORT", "5555"),
+        ('e', "LM_NAME", "abcd"),
+        ('v', "../../AI_text_detector/LMs/abcd/abcd.py:/usr/app/src/lm_name/lm_submission.py"),
+        ('v', "../../AI_text_detector/LMs/abcd/requirements.txt:/usr/app/src/lm_name/requirements.txt"),
+        ('v', "./communicator.py:/usr/app/src/communicator.py"),
+        ('v', "./server/messages.py:/usr/app/src/server/messages.py"),
+    ]
+    service_name = "communicator"
+    lm_name = 'lm_name'
+    container_name = create_container_name(service_name, lm_name)
+
+
+
+    create_or_run_container(compose_file_path, service_name, container_name, env_vars)
+    sleep(5)
+    subprocess.run(['docker-compose', '-f', compose_file_path, 'stop', container_name])
+    subprocess.run(['docker-compose', '-f', compose_file_path, 'stop', 'server'])
+
+    client.close()
+    """
