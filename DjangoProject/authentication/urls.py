1b709b710fbd0a49e65018a3dffab7d4cb1d9bac
Xela10001 <alexandre.gazur@ua.pt>
2023-06-05 16:56:25 +0100

    small change

--- DjangoProject/authentication/urls.py
+++ DjangoProject/authentication/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'authentication'
 
 urlpatterns = [
-	path('aidetector/register', views.UserRegister.as_view(), name='register'),
-	path('aidetector/login', views.UserLogin.as_view(), name='login'),
+	path('register', views.UserRegister.as_view(), name='register'),
+	path('login', views.UserLogin.as_view(), name='login'),
 	path('logout', views.UserLogout.as_view(), name='logout'),
 	path('user', views.UserView.as_view(), name='user'),
 ]
