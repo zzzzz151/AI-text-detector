0751fcdf4f539e8f2f586c2fcfd85d372816c775
Xela10001 <alexandre.gazur@ua.pt>
2023-06-05 09:15:19 +0100

    Changed IP address from localhost to 193.136.175.104

--- DjangoProject/DjangoProject/settings.py
+++ DjangoProject/DjangoProject/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
+    "193.136.175.104",
     "http://zzzzz151.pythonanywhere.com",
     "zzzzz151.pythonanywhere.com",
     'api'
@@ -58,6 +59,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
+    "http://193.136.175.104:8000"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
